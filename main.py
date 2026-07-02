20c40b6a6065def7695007d3a08f901da8160d9d
CyzmiX <itzmedigamingx@gmail.com>
2026-07-02 11:08:21 +0100

    added song search

--- main.py
+++ main.py
@@ -1,8 +1,31 @@
 from fastapi import FastAPI
 from librelyrics import LibreLyrics
+from spotapi import Song
 
 app = FastAPI()
 ll = LibreLyrics()
+
+song = Song()
+
+@app.get("/song/")
+async def get_song(q: str):
+    results = {}
+    if q:
+        songs = song.query_songs(q, limit=1)
+        song_data = songs["data"]["searchV2"]["tracksV2"]["items"][0]["item"]["data"]
+        try:
+            results = {
+                "id": song_data["id"],
+                "name": song_data["name"],
+                "album": song_data["albumOfTrack"]["name"],
+                "album_id": song_data["albumOfTrack"]["id"],
+                "cover": song_data["albumOfTrack"]["coverArt"]["sources"][-1]["url"],
+                "artists": list(map(lambda x: x["profile"]["name"], song_data["artists"]["items"]))
+            }
+        except:
+            print('Failed to get song data')
+    
+    return results
 
 @app.get("/lyrics/")
 async def get_lyrics(id: str):
